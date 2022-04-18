8e527c61f19249fb9ddd7cb615d14f1ebb963d39
Ehsan Gheychisaz <94078584+EhsanGheychisaz@users.noreply.github.com>
2022-04-19 01:35:22 +0430

    Update ehsan.js

--- js/ehsan.js
+++ js/ehsan.js
@@ -13,3 +13,10 @@ window.onclick = setInterval(function () {
   document.querySelector(".main__bodylist-featur").style.visibility = "visible";
   document.querySelector(".main--bodylist").style.left = "-3.5rem";
 }, 3000);
+var whiteMode = document.querySelector("#nav-icon3");
+whiteMode.addEventListener("click", function () {
+  document.body.style.background = "rgb(183,183,255)";
+  document.body.style.background =
+    "linear-gradient(90deg, rgba(183,183,255,1) 67%, rgba(240,250,255,1) 100%)";
+  document.querySelector("#nav--bar").style.background = "#0f9ce3";
+});
