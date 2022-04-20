06c0eeb3b3a0550b19c273c006546486fdc81c14
Ehsan Gheychisaz <94078584+EhsanGheychisaz@users.noreply.github.com>
2022-04-20 12:05:06 +0430

    Update ehsan.js

--- js/ehsan.js
+++ js/ehsan.js
@@ -12,7 +12,7 @@ document
 window.onclick = setInterval(function () {
   document.querySelector(".main__bodylist-featur").style.visibility = "visible";
   document.querySelector(".main--bodylist").style.left = "-3.5rem";
-}, 3000);
+}, 6000);
 var whiteMode = document.querySelector("#nav-icon3");
 whiteMode.addEventListener("click", function () {
   document.body.style.background = "rgb(183,183,255)";
@@ -20,3 +20,36 @@ whiteMode.addEventListener("click", function () {
     "linear-gradient(90deg, rgba(183,183,255,1) 67%, rgba(240,250,255,1) 100%)";
   document.querySelector("#nav--bar").style.background = "#0f9ce3";
 });
+window.onscroll = function () {
+  scrollfunc();
+};
+function scrollfunc() {
+  if (window.pageYOffset > 0) {
+    document.querySelector("#nav--bar").style.width = "92%";
+    document.querySelector("#nav--bar").style.margin = "3%";
+  } else {
+    document.querySelector("#nav--bar").style.margin = "0%";
+    document.querySelector("#nav--bar").style.width = "100%";
+  }
+}
+for (let i = 1; i < 6; i++) {
+  var name = "#nav_icon" + i;
+  var list = document.querySelector(`${name}`);
+  console.log(list);
+  list.addEventListener("click", function () {
+    var name1 = "#nav--textoverflow" + i;
+    document.querySelector(`${name1}`).style.height = "7rem";
+    document.querySelector(`${name1}`).style.visibility = "visible";
+    console.log(document.querySelectorAll("#nave-overflow-text"));
+    var name2 = document.querySelectorAll("#nave-overflow-text");
+    console.log(name2[i]);
+  });
+}
+var overlay = document.querySelector(".center");
+/*
+window.addEventListener("load", function () {
+  overlay.style.display = "none";
+});*/
+const loading = setInterval(() => {
+  overlay.style.display = "none";
+}, 1000);
